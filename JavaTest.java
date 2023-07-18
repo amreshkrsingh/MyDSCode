da64976803376605178dc2c7074fd60c3f8d6a80
amresh.l <amresh.l@zohocorp.com>
2023-07-18 12:55:12 +0530

    class file generated

--- JavaTest.java
+++ JavaTest.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class JavaTest {
   public static void main(String[] args) {
     Set<String> zpuidSet = new HashSet<>(Arrays.asList("34567654", "5467655342", "2435653"));
     Long[] userArray = zpuidSet.stream().map(Long::valueOf).toArray(Long[]::new);
     System.out.println("This is the result "+userArray);
-    sumnum(5+12322);
+    sumnum(5, 122);
   }
 
-  public void sumnum(int a, int b) {
-    System.out.println("The sum is " + a+b);
+  public static void sumnum(int a, int b) {
+    System.out.println("The sum is " + (a+b));
   }
 }
