4f03235cd892c0983a3488fbfb4f37c8560e1bc2
amresh.l <amresh.l@zohocorp.com>
2023-07-18 12:24:47 +0530

    changes done in javaTest.java

--- JavaTest.java
+++ JavaTest.java
@@ -2,7 +2,7 @@ public class ImportantCncept {
   public static void main(String[] args) {
     Set<String> zpuidSet = new HashSet<>(Arrays.asList("34567654", "5467655342", "2435653"));
     Long[] userArray = zpuidSet.stream().map(Long::valueOf).toArray(Long[]::new);
-    System.out.println(userArray);
+    System.out.println("This is the result " + userArray);
   }
 
 }
