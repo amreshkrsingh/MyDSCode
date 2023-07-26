25ff433d1c1cb0929bdbee28dc1ef04fe4118eb6
AMRESH KUMAR <41761275+amreshkrsingh@users.noreply.github.com>
2023-07-26 12:20:26 +0530

    Update PrimeNumber.java
    Variable name correction

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -9,30 +9,28 @@ public class PrimeNumber {
         System.out.println("Prime numbers up to " + limit + ":");
         printPrimeNumbers(limit);
     }
-
 	
-	//To improve the efficiency of the program and avoid exceeding the time limit, we can implement the SIEVE OF ERATOSTHENES ALGORITHM. 
+//To improve the efficiency of the program and avoid exceeding the time limit, we can implement the SIEVE OF ERATOSTHENES ALGORITHM. 
     private static void printPrimeNumbers(long limit) {
-        boolean[] isPrime = new boolean[(int) (limit + 1)];
-        Arrays.fill(isPrime, true);
+        boolean[] primeArray = new boolean[(int) (limit + 1)];
+        Arrays.fill(primeArray, true);
 
-        isPrime[0] = false;
-        isPrime[1] = false;
+        primeArray[0] = false;
+        primeArray[1] = false;
 
         for (int number = 2; number * number <= limit; number++) {
-            if (isPrime[number]) {
+            if (primeArray[number]) {
                 for (int multiple = number * number; multiple <= limit; multiple += number) {
-                    isPrime[multiple] = false;
+                	primeArray[multiple] = false;
                 }
             }
         }
-
-        for (int number = 2; number <= limit; number++) {
-            if (isPrime[number]) {
-                System.out.print(number + " ");
+//To print the prime numbers from the array.
+        for (int i = 2; i <= limit; i++) {
+            if (primeArray[i]) {
+                System.out.print(i + " ");
             }
         }
-
         System.out.println();
     }
 }
